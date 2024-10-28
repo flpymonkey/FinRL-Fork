caa081ad412073d8c409841d09911581f2222aac
flpymonkey <bencj96@gmail.com>
2024-10-27 17:32:08 -0700

    Updated Portfolio Optimization

---
+++ finrl/agents/portfolio_optimization/models_stable.py
@@ -0,0 +1,110 @@
+from stable_baselines3 import PPO
+
+MODELS = {"ppo": PPO}
+
+class DRLStableAgent:
+    """Implementation for DRL algorithms for portfolio optimization.
+
+    Note:
+        During testing, the agent is optimized through online learning.
+        The parameters of the policy is updated repeatedly after a constant
+        period of time. To disable it, set learning rate to 0.
+
+    Attributes:
+        env: Gym environment class.
+    """
+
+    def __init__(self, env):
+        """Agent initialization.
+
+        Args:
+            env: Gym environment to be used in training.
+        """
+        self.env = env
+
+    def get_model(
+        self, model_name, device="cpu", model_kwargs=None, policy_kwargs=None
+    ):
+        """Setups DRL model.
+
+        Args:
+            model_name: Name of the model according to MODELS list.
+            device: Device used to instantiate neural networks.
+            model_kwargs: Arguments to be passed to model class.
+            policy_kwargs: Arguments to be passed to policy class.
+
+        Note:
+            model_kwargs and policy_kwargs are dictionaries. The keys must be strings
+            with the same names as the class arguments. Example for model_kwargs::
+
+            { "lr": 0.01, "policy": EIIE }
+
+        Returns:
+            An instance of the model.
+        """
+        if model_name not in MODELS:
+            raise NotImplementedError("The model requested was not implemented.")
+
+        model = MODELS[model_name]
+        model_kwargs = {} if model_kwargs is None else model_kwargs
+        policy_kwargs = {} if policy_kwargs is None else policy_kwargs
+
+        # add device settings
+        model_kwargs["device"] = device
+        #policy_kwargs["device"] = device
+
+        # add policy_kwargs inside model_kwargs
+        model_kwargs["policy_kwargs"] = policy_kwargs
+
+        # TODO default to usethe MlpPolicy
+        return model(env=self.env, policy="MlpPolicy", **model_kwargs)
+
+    @staticmethod
+    def train_model(model, env, episodes=1):
+        """Trains portfolio optimization model.
+
+        Args:
+            model: Instance of the model.
+            episoded: Number of episodes.
+
+        Returns:
+            An instance of the trained model.
+        """
+        max_steps = len(env._df['date'].unique())
+
+        print("Max number of time steps in an episode: ", max_steps)
+
+        model.learn(
+            total_timesteps = max_steps * episodes # TODO fix this
+        )
+        return model
+
+    @staticmethod
+    def DRL_prediction(model, env, deterministic=True):
+        """make a prediction and get results"""
+        test_env, test_obs = env.get_sb_env()
+        
+        
+
+        test_env.reset()
+        max_steps = len(env._df['date'].unique())
+
+        validation_assets = None
+        validation_dates = None
+
+        for i in range(max_steps):
+            action, _states = model.predict(test_obs, deterministic=deterministic)
+
+            # Pull out the latest assets and dates
+            validation_assets = env._asset_memory["final"]
+            validation_dates = env._date_memory
+
+            # account_memory = test_env.env_method(method_name="save_asset_memory")
+            # actions_memory = test_env.env_method(method_name="save_action_memory")
+            test_obs, rewards, dones, info = test_env.step(action)
+
+            if dones[0]:
+                print("hit end!")
+                break
+        
+        return validation_assets, validation_dates
