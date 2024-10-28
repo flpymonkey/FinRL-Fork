9f1b759826c3b3160495d9ee31133e4adaa08a3c
flpymonkey <bencj96@gmail.com>
2024-10-28 15:33:34 -0700

    Added BAH model

--- finrl/agents/portfolio_optimization/models_stable.py
+++ finrl/agents/portfolio_optimization/models_stable.py
@@ -1,8 +1,8 @@
 from stable_baselines3 import PPO
 
-from .online import CRPModel
+from .online import CRPModel, BAHModel
 
-MODELS = {"ppo": PPO, "crp": CRPModel}
+MODELS = {"ppo": PPO, "crp": CRPModel, "bah": BAHModel}
 
 class DRLStableAgent:
     """Implementation for DRL algorithms for portfolio optimization.
